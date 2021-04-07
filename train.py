5fecf95d7d1be1d24ac4dc24b7daa23644e04e02
Sunghyun Jun <amiere.jun@gmail.com>
2021-04-07 18:56:10 +0900

    Add argument.

--- train.py
+++ train.py
@@ -6,6 +6,10 @@ from pytorch_lightning.loggers import NeptuneLogger
 from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.callbacks import ModelCheckpoint
 
+from datamodule import HPADataModule
+from models import HPAClassifier
+
+
 def main():
     # ----------
     # seed
@@ -15,8 +19,116 @@ def main():
     # ----------
     # args
     # ----------
-    parser = ArgumentParser()
-    parser.add_argument("--debug", action="store_true")
+    parser = ArgumentParser(description="PyTorch HPA Training")
+    parser.add_argument("--debug", action="store_true", help="debug mode")
+    parser.add_argument(
+        "--dataset-dir", default="dataset", metavar="DIR", help="path to dataset"
+    )
+    parser.add_argument(
+        "--default-root-dir",
+        default=os.getcwd(),
+        metavar="DIR",
+        help="path to checkpoint saving",
+    )
+    parser.add_argument(
+        "--gpus", type=int, default=None, help="number of GPU (default: None for CPU)"
+    )
+    parser.add_argument(
+        "--precision",
+        type=int,
+        default=32,
+        help="precision (default:32, full precision)",
+    )
+    parser.add_argument(
+        "--resume-from-checkpoint",
+        type=str,
+        default=None,
+        help="resume from checkpoint in the path",
+    )
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=32,
+        metavar="N",
+        help="input batch size for training (default: 32)",
+    )
+    parser.add_argument(
+        "--workers",
+        type=int,
+        default=2,
+        metavar="N",
+        help="how many training processes to use (default: 2)",
+    )
+    parser.add_argument(
+        "--fold-splits",
+        type=int,
+        default=5,
+        metavar="N",
+        help="number of folds (default: 5)",
+    )
+    parser.add_argument(
+        "--fold-index",
+        type=int,
+        default=0,
+        metavar="N",
+        help="fold index to use for training (default: 0)",
+    )
+    parser.add_argument(
+        "--image-size",
+        type=int,
+        default=256,
+        metavar="N",
+        help="image size for training (default: 256)",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="tf_efficientnet_b0",
+        metavar="MODEL",
+        help="name of model to train (default: 'tf_efficientnet_b0')",
+    )
+    parser.add_argument(
+        "--init-lr",
+        type=float,
+        default=1e-4,
+        metavar="LR",
+        help="learning rate (default: 0.0001)",
+    )
+    parser.add_argument(
+        "--weight-decay",
+        type=float,
+        default=1e-4,
+        metavar="DECAY",
+        help="weight decay (default: 0.0001)",
+    )
+    parser.add_argument(
+        "--max-epochs",
+        type=int,
+        default=10,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+    parser.add_argument(
+        "--alpha", type=float, default=0.25, help="focal loss alpha (default: 0.25)"
+    )
+    parser.add_argument(
+        "--gamma", type=float, default=1.5, help="focal loss gamma (default: 1.5)"
+    )
+    parser.add_argument(
+        "--neptune-logger", action="store_true", help="use neptune logger"
+    )
+    parser.add_argument(
+        "--neptune-project", type=str, default=None, help="neptune project"
+    )
+    parser.add_argument(
+        "--neptune-experiment", type=str, default=None, help="neptune experiment name"
+    )
+    parser.add_argument(
+        "--progress-bar-refresh-rate",
+        type=int,
+        default=1,
+        help="how often to refresh progress bar in steps (default: 1)",
+    )
 
     args = parser.parse_args()
 
@@ -25,39 +137,57 @@ def main():
     # ----------
     if args.debug:
         args.max_epochs = 1
+        args.batch_size = 2
         args.limit_train_batches = 3
         args.limit_val_batches = 3
 
     # ----------
     # data
     # ----------
+    dm = HPADataModule(
+        dataset_dir=args.dataset_dir,
+        batch_size=args.batch_size,
+        num_workers=args.workers,
+        fold_splits=args.fold_splits,
+        fold_index=args.fold_index,
+        image_size=args.image_size,
+    )
 
     # ----------
     # model
     # ----------
+    dm.setup()
 
-    # checkpoint_callback = ModelCheckpoint(
-    #     monitor="val_loss",
-    #     filename="xray-detector-{epoch:03d}-{val_loss:.4f}",
-    #     save_last=True,
-    #     save_top_k=3,
-    #     mode="min",
-    # )
+    model = HPAClassifier(
+        model_name=args.model,
+        init_lr=args.init_lr,
+        weight_decay=args.weight_decay,
+        max_epochs=args.max_epochs,
+        alpha=args.alpha,
+        gamma=args.gamma,
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor="valid_loss",
+        filename="hpa-clf-{epoch:03d}-{val_loss:.4f}",
+        save_last=True,
+        save_top_k=3,
+        mode="min",
+    )
 
     # ----------
     # logger
     # ----------
-    # if args.neptune_logger:
-    #     logger = NeptuneLogger(
-    #         api_key=os.environ["NEPTUNE_API_TOKEN"],
-    #         project_name=args.neptune_project,
-    #         experiment_name=args.experiment_name,
-    #         params=args.__dict__,
-    #         tags=["pytorch-lightning"],
-    #     )
-    # else:
-    #     # default logger: TenserBoardLogger
-    #     logger = True
+    if args.neptune_logger:
+        logger = NeptuneLogger(
+            api_key=os.environ["NEPTUNE_API_TOKEN"],
+            project_name=args.neptune_project,
+            experiment_name=args.neptune_experiment,
+            params=args.__dict__,
+        )
+    else:
+        # Use default logger, TenserBoardLogger
+        logger = True
 
     lr_monitor = LearningRateMonitor(logging_interval="step")
 
@@ -67,6 +197,9 @@ def main():
     trainer = pl.Trainer.from_argparse_args(
         args, callbacks=[checkpoint_callback, lr_monitor], logger=logger
     )
+
+    trainer.fit(model, dm)
+
 
 if __name__ == "__main__":
     main()
